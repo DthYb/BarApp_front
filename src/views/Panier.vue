2c3e451772b158d65cba36866226aef9b6dde442
DthYb <osiris.fd7@gmail.com>
2024-05-16 17:06:23 +0200

    feat: new cart vue

--- src/views/Panier.vue
+++ src/views/Panier.vue
@@ -10,13 +10,22 @@
             <h1 class="text-center font-bold text-[#C4B454] text-6xl">PANIER</h1>
         </div>
         <div class="flex justify-center">
-            <div class="mt-5 border-2 bg-white border-black h-[600px] w-[1000px]">
-                <div class="text-center">
-                    <h1>RÉCAPITULATIF</h1>
-                    <div class="border-2 border-black mx-auto">
-                        <h1>ETAT :</h1>
+            <div class="mt-5 border-2 flex bg-white border-black h-[600px] w-[1000px]">
+                <div class="border-r-2 border-black w-[600px] overflow-scroll overflow-x-hidden">
+                    <p>PANIER</p>
+                    <div class="border">
+                        <h1 class="font-bold text-lg">TOTAL :</h1>
+                    </div>
+                </div>
+                <div class="flex mx-auto items-center">
+                    <div class="text-center">
+                            <h1 class="text-lg font-bold">RÉCAPITULATIF</h1>
+                        <div class="border-2 border-black max-w-[400px]">
+                            <h1 class="text-left pl-2 text-lg font-bold">ETAT :</h1>
 
-                        <button>Valider mon panier</button>
+                            <button class="border border-black rounded mt-3 p-4 bg-[#C4B454] text-white text-lg">Valider mon panier</button>
+                            <img class="mx-auto" src="../assets/BarappPaiement.PNG" />
+                        </div>
                     </div>
                 </div>
             </div>
